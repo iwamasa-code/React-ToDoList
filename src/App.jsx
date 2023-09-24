236c62c748b395f0016b5dd026dc3d0431c95467
masato <69741652+iwamasa-code@users.noreply.github.com>
2023-09-24 05:35:56 +0000

    created App.jsx learning component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 const App = () => {
-    const onClickButton = () => alert()
-    const contentStyle = {
-        color: 'blue',
-        fontSize: '18px',
-    }
-    return (
-         <>
-      <h1 style={{ color: 'red' }}>こんにちは</h1>
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px",
+  };
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
       <p style={contentStyle}>お元気ですか</p>
-      <button onClick= {onClickButton}>ボタン</button>
+      <button onClick={onClickButton}>ボタン</button>
     </>
-    )
-}
+  );
+};
 
-export default App
+export default App;
